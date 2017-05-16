1828123ab65145acf93bfe2135b71f6303ac5d5a
HoangNH <you@example.com>
2017-05-16 23:39:19 +0000

    config

--- config/icho.php
+++ config/icho.php
@@ -9,6 +9,8 @@ return [
 
     'num_alert' => 10, // number rows for alert on top menu
     'upload_path' => public_path() . '/uploads/', // media_upload_path   
+	'upload_thumbs_path' => public_path() . '/uploads/thumbs/', // media_upload_path
+    'upload_thumbs_path_articles' => public_path() . '/uploads/thumbs/articles/', // media_upload_path  
     'upload_url' => $url . '/uploads/', // image path,
     'max_size_upload' => 8000000    
 ];
