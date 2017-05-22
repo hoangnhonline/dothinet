01e2ca73527119f7dcde2b4eaedc80d09c918f51
HoangNH <hoangnh@evolableasia.vn>
2017-05-22 15:06:49 +0700

    acb

--- resources/views/frontend/detail/index.blade.php
+++ resources/views/frontend/detail/index.blade.php
@@ -8,31 +8,4 @@
 @include('frontend.detail.content')
 
 @section('javascript_page')
-
-
-	<script type="text/javascript">
-		$(document).ready(function() {
-      $('.btnMuaDetail').click(function() {
-        var product_id = $(this).attr('product-id');
-        add_product_to_cart(product_id);
-      });
-
-      function add_product_to_cart(product_id) {
-        $.ajax({
-          url: "{{route('them-sanpham')}}",
-          method: "POST",
-          data : {
-            id: product_id
-          },
-          success : function(data){
-            location.href = '{{route("gio-hang")}}';
-          },
-          error : function(e) {
-            alert( JSON.stringify(e));
-          }
-        });
-      }
-
-		});
-	</script>
 @endsection
